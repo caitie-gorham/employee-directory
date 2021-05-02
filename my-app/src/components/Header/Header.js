f1e21a67cc46c94d79cec53bdb7ec4cca1fd50fe
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-01 19:13:13 -0500

    added header js file

---
+++ my-app/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <div class="p-5 text-center bg-light">
+                <h1 class="mb-3">Heading</h1>
+                <h4 class="mb-3">Subheading</h4>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
