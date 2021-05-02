7d80b2ad95a9f304a05d415507d039192ca36b93
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-01 21:32:12 -0500

    changed file structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ function Header() {
     return (
         <header>
             <div class="p-5 text-center bg-light">
-                <h1 class="mb-3">Heading</h1>
-                <h4 class="mb-3">Subheading</h4>
+                <h1 class="mb-3">Employee Directory</h1>
+                <h4 class="mb-3">React App</h4>
             </div>
         </header>
     );
