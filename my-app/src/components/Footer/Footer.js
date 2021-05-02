a1dc005ad8fbb6e9138448e3e8eb0dd6dceec577
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-05-01 19:11:25 -0500

    added footer js file

---
+++ my-app/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2021 Copyright:
+            <a class="text-dark" href="https://github.com/caitie-gorham">Caitie Gorham</a>
+        </div>
+    );
+};
+
+export default Footer;
